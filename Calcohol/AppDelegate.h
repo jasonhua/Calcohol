d4daf5a79647235729f4b5824fe38a9608a7218a
Jason Hua <jayhua333@gmail.com>
2015-07-12 13:20:27 -0700

    Initial Commit

---
+++ Calcohol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calcohol
+//
+//  Created by Jason Hua on 7/12/15.
+//  Copyright (c) 2015 JJSports. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calcohol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calcohol
+//
+//  Created by Jason Hua on 7/12/15.
+//  Copyright (c) 2015 JJSports. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
